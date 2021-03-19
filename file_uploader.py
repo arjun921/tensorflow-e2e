28bcdc91e3c7a1b0b79662a4eda51c99d9575d8a
Arjun Sunil <arjun921@gmail.com>
2021-03-19 15:42:47 +0530

    Beautified

--- file_uploader.py
+++ file_uploader.py
@@ -1,67 +1,75 @@
-import streamlit as st
+import os
 import uuid
 from pathlib import Path
-from configs.config import config
-import os
-from utils.helpers import underscore_seperated_path, get_model_dir
+
 import semver
+import streamlit as st
 
-model_version = semver.VersionInfo.parse('1.0.0')
+from configs.config import config
+from utils.helpers import get_model_dir, underscore_seperated_path
 
-st.header('Data Uploader')
+model_version = semver.VersionInfo.parse("1.0.0")
+
+st.header("Data Uploader")
 workspace_exists = os.path.exists(config.WORKSPACE)
 
 # Check workspace exists
 try:
     assert workspace_exists
 except Exception as e:
-    st.error('Error: Local workspace not found. Did you setup enviornment with `make init`?')
+    st.error(
+        "Error: Local workspace not found. Did you setup enviornment with `make init`?"
+    )
 
-if workspace_exists: # dont render UI without workspace init (helps reduce multiple folder exists checks)
+if (
+    workspace_exists
+):  # dont render UI without workspace init (helps reduce multiple folder exists checks)
     data = {}
 
     # Input fields
-    model_name = Path(st.text_input('Enter Model Name'))
-    class_count = st.number_input("How many classes?", 1, 1000,value=2)
-
+    model_name = Path(st.text_input("Enter Model Name"))
+    class_count = st.number_input("How many classes?", 1, 1000, value=2)
 
     # Save values in URL
-    st.experimental_set_query_params(class_count=class_count,model_name=model_name)
+    st.experimental_set_query_params(class_count=class_count, model_name=model_name)
 
     # Retrieve app state
     app_state = st.experimental_get_query_params()
     # print(app_state)
 
-
     # Display saved result if it exist
     if "class_count" in app_state:
         class_count = app_state["class_count"][0]
         for class_x in range(int(class_count)):
-            st.subheader('Upload Class')
-            class_name = st.text_input('Class Name',key=class_x)
-            data[class_name] = st.file_uploader('Upload images', key=class_x, accept_multiple_files=True)
-            st.text('---')
+            st.subheader("Upload Class")
+            class_name = st.text_input("Class Name", key=class_x)
+            data[class_name] = st.file_uploader(
+                "Upload images", key=class_x, accept_multiple_files=True
+            )
+            st.text("---")
     else:
         st.write("No result to display, compute a value first.")
 
-    save_files = st.button('Save Data')
-
+    save_files = st.button("Save Data")
 
     if save_files:
         local_model_path = underscore_seperated_path(str(model_name))
         model_path = config.WORKSPACE / local_model_path
-        model_dir,model_version = get_model_dir(path=model_path,version= model_version)
-        st.experimental_set_query_params(class_count=class_count, model_name=local_model_path, model_version=model_version)
+        model_dir, model_version = get_model_dir(path=model_path, version=model_version)
+        st.experimental_set_query_params(
+            class_count=class_count,
+            model_name=local_model_path,
+            model_version=model_version,
+        )
 
         # Create model folder in workspace
         try:
-            os.makedirs(model_dir,exist_ok=True)
+            os.makedirs(model_dir, exist_ok=True)
         except Exception as e:
             st.error(e)
 
         # TODO: Loop and create class subfolders
         try:
-            # class_count = st.experimental_get_query_params()['class_count'][0]
             classes = data.keys()
             for class_ in classes:
                 class_path = model_dir / Path(class_)
@@ -70,14 +78,13 @@ if workspace_exists: # dont render UI without workspace init (helps reduce multi
                     if file is not None:
                         file_path = class_path / Path(file.name)
                         bytes_data = file.getvalue()
-                        fopen = open(file_path,'wb')
+                        fopen = open(file_path, "wb")
                         fopen.write(bytes_data)
                         fopen.close()
-                        # st.write(bytes_data)
         except Exception as e:
             st.error(e)
         # TODO: Save image files to class subfolders
-        st.success(f'Data saved successfully for {model_name} v{model_version} 🎊')
-        st.text('To start training, run')
-        st.code('make train')
+        st.success(f"Data saved successfully for {model_name} v{model_version} 🎊")
+        st.text("To start training, run")
+        st.code("make train")
         print(data)
